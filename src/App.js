2ba779a7625c3df0d6caaef59bec3a6df3bc0fcd
Sandra Adey <sandraadeyinka@gmail.com>
2023-10-14 16:40:55 +0100

    styles

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import Dicionary from "./components/Dicionary";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <h1 className="heading">Dictionary</h1>
+        </header>
+        <main>
+          <Dicionary defaultKeyword="aesthetic" />
+        </main>
+        <footer className="mt-5 footer">
+          <p className="m-0 credit">
+            Coded by{" "}
+            <a href="https://github.com/s-shemmee" target="_blank">
+              {" "}
+              Chaimae Lamirine
+            </a>
+            ,
+            <a
+              href="https://github.com/shemmee/Dictionary-React-App"
+              target="_blank"
+            >
+              Open sourced on Github.
+            </a>{" "}
+            Hosted on Vercel
+          </p>
+        </footer>
+      </div>
     </div>
   );
 }
